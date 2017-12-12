1640bf9ea1310dcbdb85e55055a69aa7c0fe5bbe
Alan Murtagh <alan.murtagh@sisa-tech.com>
2017-12-12 12:22:58 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/fogleman/primitive/primitive"
+	"github.com/gorilla/mux"
+	"github.com/nfnt/resize"
+)
+
+var (
+	src         = "."
+	processed   = "processed"
+	placeholder = "placeholder.png"
+)
+
+func main() {
+
+	go processPictures()
+
+	handler := mux.NewRouter()
+	handler.HandleFunc("/{path:.*}", handle)
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func processPictures() {
+	os.RemoveAll(processed)
+
+	err := os.MkdirAll(processed, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fis, err := ioutil.ReadDir(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fi := range fis {
+		err = process(filepath.Join(src, fi.Name()), filepath.Join(processed, fi.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Printf("All files processed\n")
+}
+
+func process(inpath, outpath string) error {
+
+	input, err := primitive.LoadImage(inpath)
+	if err != nil {
+		if err == image.ErrFormat {
+			fmt.Printf("Skipping file '%s': %v\n", inpath, err)
+			return nil
+		}
+		return err
+	}
+
+	fmt.Printf("Processing file: %s", inpath)
+
+	size := uint(256)
+
+	input = resize.Thumbnail(size, size, input, resize.Bilinear)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// background color
+	bg := primitive.MakeColor(primitive.AverageImageColor(input))
+
+	// run algorithm
+	outputSize := 1024
+	workers := 1
+
+	model := primitive.NewModel(input, bg, outputSize, workers)
+	frame := 0
+
+	count := 100
+
+	Nth := 1
+
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		return err
+	}
+	output := tmp.Name()
+	defer os.Remove(output)
+	tmp.Close()
+
+	mode := 1
+	alpha := 128
+	repeat := 0
+
+	// fmt.Printf("temp file: %s\n", tmp.Name())
+
+	for i := 0; i < count; i++ {
+		frame++
+
+		// fmt.Printf("frame: %v\n", i)
+
+		// find optimal shape and add it to the model
+		model.Step(primitive.ShapeType(mode), alpha, repeat)
+
+		// write output image(s)
+
+		ext := strings.ToLower(filepath.Ext(output))
+		percent := strings.Contains(output, "%")
+		saveFrames := percent && ext != ".gif"
+		saveFrames = saveFrames && frame%Nth == 0
+		last := i == count-1
+		var path string
+		if saveFrames || last {
+			path = outpath
+			if percent {
+				path = fmt.Sprintf(outpath, frame)
+			}
+			err = primitive.SavePNG(path, model.Context.Image())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	fmt.Printf("  COMPLETE\n")
+	return nil
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+
+	path := mux.Vars(r)["path"]
+
+	path = filepath.Join(processed, path)
+
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			var fi os.FileInfo
+			fi, err = os.Stat(filepath.Join(src, mux.Vars(r)["path"]))
+			if err == nil {
+				if !fi.IsDir() {
+					f, err = os.Open(placeholder)
+					if err != nil {
+						code := http.StatusInternalServerError
+						http.Error(w, http.StatusText(code), code)
+						return
+					}
+					defer f.Close()
+
+					_, err = io.Copy(w, f)
+					if err != nil {
+						code := http.StatusInternalServerError
+						http.Error(w, http.StatusText(code), code)
+						return
+					}
+
+					return
+				}
+			}
+			code := http.StatusNotFound
+			http.Error(w, http.StatusText(code), code)
+			return
+		}
+		code := http.StatusInternalServerError
+		http.Error(w, http.StatusText(code), code)
+		return
+	}
+	defer f.Close()
+
+	_, err = io.Copy(w, f)
+	if err != nil {
+		code := http.StatusInternalServerError
+		http.Error(w, http.StatusText(code), code)
+		return
+	}
+
+}
